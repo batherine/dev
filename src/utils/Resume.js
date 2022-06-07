7fae89e7dd785803de364e640b216fdd337db0f8
batherine <catherinedougherty00@gmail.com>
2022-06-07 13:53:01 -0600

    edit

--- src/utils/Resume.js
+++ src/utils/Resume.js
@@ -4,11 +4,12 @@ export const Work = [
         employer: "WellSky",
         dates: "Oct 2019-Present",
         location: "Remote",
-        descriptions: ["Sole engineer tasked with resiliency, performance, and scalability research for core application",
-            "Worked with IBM lab services to identify processes within the core application requiring optimization",
-            "Created in depth documentation on improved backend development and SQL practices",
-            "Integrated and maintained New Relic service reporting with core application and client servers",
-            "Instrumental in production server monitoring alert and dashboard set up for critical failures due to cpu utilization, memory usage, network latency, application performance, etc"
+        descriptions: ["Sole engineer tasked with resiliency, performance, and scalability research for the Specialty Care application",
+            "Worked with IBM lab services to identify processes within the core application that require optimization",
+            "Created in-depth documentation on best practices for general backend development and use of SQL",
+            "Integrated and maintained New Relic service reporting for client servers ",
+            "Implemented various custom New Relic integrations using javascript, Java, GraphQL, and bash",
+            "Instrumental in server monitoring alerts and dashboard set up for critical failures related to cpu utilization, memory usage, network latency, application performance, etc."
         ]
     },
     {
@@ -16,8 +17,9 @@ export const Work = [
         employer: "HCS (Acquired by WellSky)",
         dates: "Jul 2017-Oct 2019",
         location: "Wall, NJ",
-        descriptions: ["Practiced agile development through code enhancements for Revenue Cycle module of application",
-            "Gained comprehensive knowledge of long term care and skilled nursing business operations thus translating into judicious and efficient code enhancements",
+        descriptions: ["Implemented code enhancements and bug fixes for the application’s Revenue Cycle module",
+            "Practiced agile development with releases going out at the end of each two week sprint",
+            "Gained comprehensive knowledge of long term care and skilled nursing business operations ",
             "Provided clients with support on technical aspects of core application"
         ]
     }
@@ -76,6 +78,11 @@ export const Skills = [{
 {
     type: "language",
     value: "NRQL",
+    color: "teal"
+},
+{
+    type: "language",
+    value: "GraphQL",
     color: "teal"
 },
 {
